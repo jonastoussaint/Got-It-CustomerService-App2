3ccac7602aff762ec8b82c90a4ce1571c7bb3901
Markeya Banks <markeyab@ymail.com>
2020-03-10 15:25:42 -0400

    OnClick RecyclerView

---
+++ app/src/main/java/com/example/testproject/Tickets.java
@@ -0,0 +1,46 @@
+package com.example.testproject;
+
+import com.parse.ParseClassName;
+import com.parse.ParseObject;
+
+import java.util.Date;
+
+@ParseClassName("SupportTicket")
+
+public class Tickets extends ParseObject {
+
+    public static final String KEY_TICKETID="objectId";
+    public static final String KEY_DATE="createdAt";
+    public static final String KEY_TYPE="sup_complainant_type";
+    public static final String KEY_STATUS="sup_status";
+
+    public String getKeyObjid(){
+        return getString(KEY_TICKETID);
+    }
+
+
+    public Date getDate(){
+
+        return getCreatedAt();
+    }
+
+    public void setDate(String date){
+        put(KEY_DATE, date);
+    }
+
+    public String getType(){
+        return getString(KEY_TYPE);
+    }
+
+    public void setType(String type){
+        put(KEY_TYPE, type);
+    }
+
+    public String getStatus(){
+        return getString(KEY_STATUS);
+    }
+
+    public void setStatus(String status){
+        put(KEY_STATUS, status);
+    }
+}
